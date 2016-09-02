0e3b99bf7dafd28348dd78248c6fd1e768b76958
wayne <w@MacBook-Air.local>
2016-09-02 08:17:59 -0700

    add idea scoffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160901045045) do
+ActiveRecord::Schema.define(version: 20160902150438) do
+
+  create_table "ideas", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string   "name"
